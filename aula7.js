f966e5f5ba5671caa6d6d49aa0cb95ce38ee88c8
Leonardo <leonardo.palh4no@gmail.com>
2024-08-07 20:35:58 -0300

    add comparedor de 3 fatores

---
+++ aula7.js
@@ -0,0 +1,57 @@
+function ex24()
+{
+
+    var n1 = num1.value;
+    var n2 = num2.value;
+    var n3 = num3.value;
+
+    var resultado1;
+    var resultado2;
+//>
+    if       (n1 > n2)
+    {
+        if (n1 > n3)
+        {
+            resultado1 = "O maior é " + n1
+        }
+        else
+        {
+            resultado1 = "O maior é " + n3
+        }
+
+    }else 
+    {
+        if (n2 > n3)
+        {
+            resultado1 = "O maior é " + n2
+        }else 
+        {
+             resultado1 = "O maior é " + n3
+        }
+    }
+    //<
+    if       (n1 < n2)
+        {
+            if (n1 < n3)
+            {
+                resultado2 = " e o menor é " + n1
+            }
+            else
+            {
+                resultado2 = " e o menor é " + n3
+            }
+    
+        }else 
+        {
+            if (n2 > n3)
+            {
+                resultado2 = " e o menor é " + n2
+            }else 
+            {
+                resultado2 = " e o menor é " + n3
+            }
+        }
+    
+   saida.value = resultado1 + resultado2
+
+}
